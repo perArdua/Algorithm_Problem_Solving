c5e6f348640baac40e03571ff0559e78c2a42bd2
Tae Ho Kim <kok8454@naver.com>
2020-09-17 21:31:06 +0900

    점화식 재정의를 통하여 메모이제이션을 적용시킴 정답코드

--- BOJ_2225_decompositeSum.cpp
+++ BOJ_2225_decompositeSum.cpp
@@ -6,17 +6,16 @@ using namespace std;
 const long long MOD = 1000000000;
 int n, k;
 
-long long cache[200][200 * 200]; // cache[picked][total]
+long long cache[201][201]; // cache[n][k]
 
-long long solve(int picked, int tot){
-    if(picked == k) return tot == n ? 1 : 0;
-
-    long long& ret = cache[picked][tot];
+long long solve(int curSum, int toPick){
+    if(curSum < 0) return 0;
+    if(toPick == 1) return 1;
+    long long& ret = cache[curSum][toPick];
     if(ret != -1) return ret;
-
     ret = 0;
-    for(int next = 0; next <= n; ++next){
-        ret += solve(picked + 1, tot + next);
+    for(int i = 0; i <= n; ++i){
+        ret += solve(curSum - i, toPick - 1);
         ret %= MOD;
     }
     return ret;
@@ -28,7 +27,7 @@ int main(){
     cout.tie(NULL);
     memset(cache, -1, sizeof(cache));
     cin >> n >> k;
-    long long res = solve(0, 0);
+    long long res = solve(n, k);
     cout << res << endl;
     return 0;
 }
