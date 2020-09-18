fbbf71bf6d18e62f8a231c0b1f6b97adcf4da5a4
Tae Ho Kim <kok8454@naver.com>
2020-09-18 18:42:52 +0900

    BOJ 1010문제를 기존의 생각했던 방법을 버리고 단순한 방법을 생각하여 다시 풀었다.

--- BOJ_1010_makeBriedge.cpp
+++ BOJ_1010_makeBriedge.cpp
@@ -3,10 +3,14 @@
 #define endl "\n"
 
 using namespace std;
-int cache[30][30];
+int cache[31][31];
+
 /*
-m개중 맨 위의 1개를 선택했다면 나머지 m-1개에서 n-1개를 선택한다
-n-1 C r-1 + n-1 C r
+좌측에서 우측으로 즉 N에 대응 되는 M을 계산하려고 하니 비슷한 동작을 
+수행하는 재귀함수를 또 만들어야 하는 번거로움이 있었다.
+문제를 단순화 하기 위해서 우측에서 좌측으로 즉 M에 대응 되는 N을 선택하면
+교차되는지 여부의 관계 없이, 그 경우는 반드시 1가지이다. (자동배치)
+즉 우리는 mCn을 계산하면 문제의 정답을 구할 수 있다.
 */
 int preCalc(int n, int m){
     if(n == m || m == 0) return 1;
@@ -15,10 +19,6 @@ int preCalc(int n, int m){
     return ret = preCalc(n-1, m-1) + preCalc(n-1, m);
 }
 
-int solve(int n, int m){
-    
-
-}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -26,10 +26,11 @@ int main(){
     cout.tie(NULL);
     memset(cache, -1, sizeof(cache));
     int t;
+    cin >> t;
     while(t--){
         int n, m;
         cin >> n >> m; // 0 < n <= m < 30
-        solve(n, m);
+        cout << preCalc(m, n) << endl;
 
     }
 
