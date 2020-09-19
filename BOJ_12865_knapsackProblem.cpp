d39ca5b02f35be28771ff8d7662e16ef869bd4e0
Tae Ho Kim <kok8454@naver.com>
2020-09-19 21:09:37 +0900

    12865 cache에서랑 knapsack 함수 내에서 두번째 인덱스를 서로 다르게 사용하여 배열 인덱스를 초과하는 문제로 인하여 런타임 에러가 나는 것을 수정하였다.

--- BOJ_12865_knapsackProblem.cpp
+++ BOJ_12865_knapsackProblem.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-
+#include <cstring>
 #define endl "\n"
 
 using namespace std;
 int n, k;
 int weight[100], value[100];
-int cache[100][1001];
+int cache[100][100001];
 
-inline int max(int a, int b){
+int max(int a, int b){
     if(a > b) return a;
     return b;
 }
@@ -17,18 +17,15 @@ knapsack(item, capacity) = knapsack(item + 1, capacity + weight[item]) + value[i
 */
 int knapsack(int item, int weights){
     if(item == n) return 0;
+    
     int& ret = cache[item][weights];
     if(ret != 0) return ret;
+    
     ret = knapsack(item + 1, weights);
     if(weights + weight[item] <= k){
         ret = max(ret, knapsack(item + 1, weights + weight[item]) + value[item]);
     }
-    // for(int nextItem = item + 1; nextItem < n; ++nextItem){
-    //     ret = max(ret, knapsack(nextItem, weights));
-    //     if(weights + weight[item] <= k){
-    //         ret = max(ret, knapsack(nextItem, weights + weight[item]) + value[item]);
-    //     }    
-    // }
+    
     return ret;
 }
 
@@ -36,6 +33,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    memset(cache, 0, sizeof(cache));
     cin >> n >> k;
     for(int i = 0; i < n; ++i){
         cin >> weight[i] >> value[i];
