a0abcadca311c08db252ffbc52d1566e8de353d4
Tae Ho Kim <kok8454@naver.com>
2020-09-23 19:44:09 +0900

    실수 했던 부분은 stickers의 개수는 최대 해당 스티커의 row * col을 가질 수 있는데 이를 맨 처음부터 고려하지 않아서 어디가 잘못됐는지 파악하는데 오랜 시간이 걸렸다

--- BOJ_18808_attachStickers.cpp
+++ BOJ_18808_attachStickers.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 #include <cstring>
 #include <fstream> // test
+#include <string>
 #define endl "\n"
 #define MAX_ROTATION 4
 using namespace std;
-int screen[40][40];
-vector<vector<pair<int, int> > > stickers(100, vector<pair<int, int>>(10));
+int screen[41][41];
+vector<vector<pair<int, int> > > stickers(100, vector<pair<int, int>>(100));
 int r, c, k;
+ofstream out("test.txt"); //test
 
 void makeStickers(){
     int row, col, tmp;
@@ -27,6 +29,7 @@ void makeStickers(){
             }
         }
         stickers[type].resize(decideResize);
+        stickers[type].push_back(make_pair(row, col));
     }
 }
 int countAttachedStickers(){
@@ -41,12 +44,15 @@ int countAttachedStickers(){
 bool inRange(int y, int x){
     return (y >= 0 && y < r) && (x >= 0 && x < c);
 }
-bool set(int type, int delta, int y, int x){
+bool set(const int type, const int delta, const int y, const int x){
     bool ret = true;
-    for(int i = 0; i < stickers[type].size(); ++i){
-        const int ny = stickers[type][i].first;
-        const int nx = stickers[type][i].second;
-        if(!inRange(ny, nx)) ret = false;
+    for(int i = 0; i < stickers[type].size() - 1; ++i){ // 맨 마지막에는 해당 스티커의 row, col 정보가 들어 있으므로 이를 제외한다.
+        const int ny = y + stickers[type][i].first;
+        const int nx = x + stickers[type][i].second;
+        if(!inRange(ny, nx)) {
+            ret = false; 
+            out << "(nx, ny) : " + to_string(nx) + " " + to_string(nx) << endl;
+        }
         else if((screen[ny][nx] += delta) > 1) ret = false;
     }
     return ret;
@@ -54,81 +60,62 @@ bool set(int type, int delta, int y, int x){
 // range checking이 된다. 만약 -1, -1 페어를 리턴했다는 것은
 // 1. out-of-range error 이거나 2. 빈칸을 정말로 못찾았다는 뜻이다.
 // case 1, 2 모두 다음 스티커로 넘어가야함을 의미한다.
-pair<int, int> findNextBlank(int y, int x){ 
-    int ry = -1, rx = -1;
-    for(int i = y; i < r; ++i){
-        for(int j = x; j < c; ++j){
-            if(!screen[i][j]){
-                ry = i, rx = j; break;
-            }
-        }
-        if(ry != -1) break;
-    }
-    return make_pair(ry, rx);
-}
-// void rotation(){
-//     int tmp[40][40];
-//     for(int i = 0; i < r; ++i){
-//         for(int j = 0; j < c; ++j){
-//             tmp[i][j] = screen[i][j];
+// pair<int, int> findNextBlank(int y, int x){ 
+//     int ry = -1, rx = -1;
+//     for(int i = y; i < r; ++i){
+//         for(int j = x; j < c; ++j){
+//             if(!screen[i][j]){
+//                 ry = i, rx = j; break;
+//             }
 //         }
+//         if(ry != -1) break;
 //     }
-//     for(int i = 0; i < c; ++i){
-//         for(int j = 0; j < r; ++j){
-//             screen[i][j] = tmp[r -1 - j][i];
-//         }
-//     }
-//     swap(r, c);
+//     return make_pair(ry, rx);
 // }
-// r = 3, c = 4
-// 20 -> 00
-// 12 -> 21
-// 03 -> 32
-// 02 -> 22
-// cal col = abs(r - storedValue)
-// 11 -> 11
-void rotation(int type){
-    for(int i = 0; i < stickers[type].size(); ++i){
-        int tmpY = stickers[type][i].first;
-        int tmpX = stickers[type][i].second;
-        stickers[type][i].second = tmpX;
-        stickers[type][i].first = abs(r - 1 - tmpY);
-    }
-    // swap(r, c);
 
+void rotation(const int type){ // 90도 C.W. 회전
+    for(int i = 0; i < stickers[type].size() - 1; ++i){ // 맨 마지막에는 해당 스티커의 row, col 정보가 들어 있으므로 이를 제외한다.
+        swap(stickers[type][i].first, stickers[type][i].second);
+        stickers[type][i].second = stickers[type].back().first - 1 - stickers[type][i].second;
+    }
+    swap(stickers[type].back().first, stickers[type].back().second);
 }
 
 
 int cover(){
-    ofstream out("test.txt"); //test
     for(int type = 0; type < k; ++type){
         bool isPasted = false;
-        auto p = findNextBlank(0, 0);
-        while(p.first != -1 && !isPasted){
+        // auto p = findNextBlank(0, 0); // 스티커가 안붙어 있는 칸을 찾는다.
+        auto p = make_pair(0, 0);
+        while(p.first != -1 && !isPasted){// 스티커가 붙거나, range를 벗어나면 while문을 탈출하여 다음 type을 시도한다
             for(int rotate = 0; rotate < MAX_ROTATION; ++rotate){
-                out << "type : " << type << endl;
+                out << "type : " << type << endl;//test start
                 out << "rotation : " << rotate << endl;
                 out << "Blank ( x, y ) = " << "(" << p.second << ", " << p.first << ")" << endl;
                 out << "sticker : " << endl;
-                for(int i = 0; i < stickers[type].size(); ++i){
+                for(int i = 0; i < stickers[type].size() - 1; ++i){
                     out << "(x, y) = " << "(" << stickers[type][i].second << ", " << stickers[type][i].first << ")" << endl;
                 }
-                for(int i = 0; i < r; ++i){//test s
+                for(int i = 0; i < r; ++i){
                     for(int j = 0 ; j < c; ++j){
                         out << screen[i][j] << " ";
                     }
                     out << endl;
                 }
-                out << endl; // test e
-                if(set(type, 1, p.first, p.second)){
+                out << endl; // test end
+                if(set(type, 1, p.first, p.second)){ //screen에 stkicker를 붙인다.
                     isPasted = true;
                     break;
                 }else{
-                    set(type, -1, p.first, p.second);
-                    rotation(type);
+                    set(type, -1, p.first, p.second); // 만약 sticker가 붙지 않았다면 위에 if에서 붙였던 부분을 떼버린다.
+                    rotation(type); // 해당 스티커를 회전시킨다.
                 }
             }
-            p = findNextBlank(p.first, p.second + 1);
+            if(p.second + 1 < c) p = make_pair(p.first, p.second + 1);
+            else p = make_pair(p.first + 1, 0);
+            
+            if(!inRange(p.first, p.second)) {out << "!inRange executed " + to_string(p.second) + " " + to_string(p.first) << endl; break;}
+            // p = findNextBlank(p.first + 1, 0);
         }
     }
     int ret = countAttachedStickers();
