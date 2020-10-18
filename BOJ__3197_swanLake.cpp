47fe17065301b80bbea7171a1147f0b425732669
Tae Ho Kim <kok8454@naver.com>
2020-10-18 21:07:23 +0900

    3197 swan의 movement를 next step으로 옮길 때, check를 true로 하지 않는 불상사가 발생하여 오류를 고치는데 참 오래걸렸다.

--- BOJ__3197_swanLake.cpp
+++ BOJ__3197_swanLake.cpp
@@ -3,10 +3,6 @@
 #include <vector>
 #include <queue>
 #define endl "\n"
-#define water 0
-#define ice 1
-#define swan 2
-#define visited 3
 using namespace std;
 
 const int dy[] = {-1, 1 ,0 ,0};
@@ -18,61 +14,26 @@ struct Point{
 };
 
 int row, col;
-int map[1500][1500];
-
+char map[1500][1500];
+bool check[1500][1500];
 vector<Point> swanPos;
 queue<Point> waterQ;
 queue<Point> g;
+
 inline bool inRange(int y, int x){
     return ((y >= 0 && y < row) && (x >= 0 && x < col));
 }
 
 void input(){
-    for(int i = 0; i < row; ++i){
-        string tmp;
-        cin >> tmp;
-        for(int j = 0; j < tmp.size(); ++j){
-            switch (tmp[j]){
-                case '.':
-                    map[i][j] = water;
-                    break;
-                case 'X':
-                    map[i][j] = ice;
-                    break;
-                case 'L':
-                    map[i][j] = swan;
-                    swanPos.push_back(Point(i, j));
-                    break;
-                default:
-                    cout << "wrong input" << endl;
-                    exit(0);
-                    break;
-            }
-        }
-    }
-}
-
-void output(){
+    cin >> row >> col;
     for(int i = 0; i < row; ++i){
         for(int j = 0; j < col; ++j){
-            cout << map[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void findFirstAdjacent(){
-    for(int i = 0; i < row; ++i){
-        for(int j = 0; j < col; ++j){
-            if(map[i][j] != ice){
-                for(int dir = 0; dir < 4; ++dir){
-                    const int ny = i + dy[dir];
-                    const int nx = j + dx[dir];
-                    if(map[ny][nx] == ice){
-                        waterQ.push(Point(i, j));
-                        break;
-                    }
-                }
+            cin >> map[i][j];
+            if(map[i][j] == 'L'){
+                swanPos.push_back(Point(i, j));
+                waterQ.push(Point(i, j));
+            }else if(map[i][j] == '.'){
+                waterQ.push(Point(i, j));
             }
         }
     }
@@ -87,8 +48,8 @@ void melting(){
             const int ny = y + dy[dir];
             const int nx = x + dx[dir];
             if(!inRange(ny, nx)) continue;
-            if(map[ny][nx] == ice){
-                map[ny][nx] = water;
+            if(map[ny][nx] == 'X'){
+                map[ny][nx] = '.';
                 tmp.push(Point(ny, nx));
             }
         }
@@ -98,26 +59,27 @@ void melting(){
 
 bool isSameSection(){
     queue<Point> nextStep;
-    
     while(!g.empty()){
         Point tmp = g.front(); g.pop();
         int y = tmp.y, x = tmp.x;
         for(int dir = 0; dir < 4; ++dir){
-            const int ny = y + dir[dy];
-            const int nx = x + dir[dx];
-            if(!inRange(ny, nx)) continue;
-            if(map[ny][nx] == ice){
-                nextStep.push(Point(ny, nx));
-                continue;
-            }
-            if(map[ny][nx] == visited) continue;
-            /* inRange and isWater and isVisited  */
-            if(ny == swanPos[1].y && nx == swanPos[1].x){
+            const int ny = y + dy[dir];
+            const int nx = x + dx[dir];
+            
+            if((ny == swanPos.back().y) && (nx == swanPos.back().x)){
                 return true;
-            }else{
-                g.push(Point(ny, nx));
-                map[ny][nx] = visited;
             }
+            
+            if(!inRange(ny, nx) || check[ny][nx]) continue;
+            if(map[ny][nx] == 'X'){
+                nextStep.push(Point(ny, nx));
+                check[ny][nx] = true;
+                continue;
+            }else if(map[ny][nx] == '.'){
+                g.push(Point(ny, nx));
+                check[ny][nx] = true;
+            }
+            
         }
     }
     g = move(nextStep);
@@ -127,9 +89,10 @@ bool isSameSection(){
 
 int dfsSolve(){
     int retDays = 0;
-    findFirstAdjacent();
     while(true){
-        if(isSameSection()) {break;}
+        if(isSameSection()){
+            break;
+        }
         else{
             melting();
             ++retDays;
@@ -140,14 +103,13 @@ int dfsSolve(){
 
 void initStart(){
     g.push(swanPos[0]);
-    map[swanPos[0].y][swanPos[0].x] = visited;
+    check[swanPos[0].y][swanPos[0].x] = true;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> row >> col;
     input();
     initStart();
     cout << dfsSolve() << endl;
