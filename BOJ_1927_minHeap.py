93716ffb280e093e1da9b86f2eb9b1e331bc5568
Tae Ho Kim <kok8454gmail.com>
2021-07-07 00:48:52 +0900

    1927 heap

---
+++ BOJ_1927_minHeap.py
@@ -0,0 +1,14 @@
+from sys import stdin
+import heapq
+input = stdin.readline
+MIS = lambda: map(int, input().rstrip().split())
+MIRS = lambda: map(int, stdin.readlines())
+
+arr = []
+
+data = list(MIRS())
+for d in data[1:]:
+    if not d:
+        print(heapq.heappop(arr) if arr else 0)
+    else:
+        heapq.heappush(arr, d)
