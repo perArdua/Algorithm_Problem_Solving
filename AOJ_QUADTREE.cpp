b580bcbc3120e5c2cf14b6383ef10b783860e991
Tae Ho Kim <kok8454gmail.com>
2021-07-28 00:49:33 +0900

    add contents

--- AOJ_QUADTREE.cpp
+++ AOJ_QUADTREE.cpp
@@ -11,7 +11,28 @@
 
 using namespace std;
 
+string solve(string::iterator& it){
+    char head = *it++;
+    if(head == 'b' || head == 'w'){
+        return string(1, head);
+    }
+    string upperLeft = solve(it);
+    string upperRight = solve(it);
+    string lowerLeft = solve(it);
+    string lowerRight = solve(it);
+    return "x" + lowerLeft + lowerRight + upperLeft + upperRight;
+}
+
 int main(){
     FAST;
+    int t;
+    cin >> t;
+    rep(tc, 0, t){
+        string s;
+        cin >> s;
+        string::iterator iter= s.begin();
+        cout << solve(iter) << endl;
+    }
+
     return 0;
 }
