534ad4fab6799357844143a61dccf1b56936b34f
Kim Tae Ho <kok8454@gmail.com>
2021-08-23 02:09:47 +0900

    advanced version of LIS, O(nlgn)

---
+++ AOJ/AOJ_LIS_ADVANCED.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define Min(X, Y) ((X) < (Y) ? (X) : (Y))
+#define Max(X, Y) ((X) > (Y) ? (X) : (Y))
+#define pii pair<int, int>
+#define pb push_back
+#define INF 987654321
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(){
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    int t;
+    cin >> t;
+    rep(tc, 0, t){
+        int n;
+        cin >> n;
+        vector<int> sq(n);
+        for(auto& e : sq) cin >> e;
+        vector<int> res;
+        res.pb(-INF);
+        rep(i, 0, n){
+            if(res.back() < sq[i]){
+                res.pb(sq[i]);
+            }else{
+                *lower_bound(res.begin(), res.end(), sq[i]) = sq[i];
+            }
+        }
+        cout << res.size() - 1 << endl;
+    }
+    return 0;
+}
