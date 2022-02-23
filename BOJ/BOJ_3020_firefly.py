5342886f7d0f0282d2dceaeffb500b0ecf72ea9b
perArdua <70432496+perArdua@users.noreply.github.com>
2022-02-24 02:40:18 +0900

    imos

---
+++ BOJ/BOJ_3020_firefly.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+input = stdin.readline
+MIS = lambda: map(int, input().rstrip().split())
+MIRS = lambda: map(int, stdin.readlines())
+
+n, h = MIS()
+
+arr = [0] * h
+
+for i in range(n):
+    if i & 1:
+        arr[0] += 1
+        arr[int(input().rstrip())] -= 1
+    else:
+        arr[h - int(input().rstrip())] += 1
+        
+for i in range(1, h):
+    arr[i] += arr[i - 1]
+    
+res = min(arr)
+cnt = arr.count(res)
+
+print(res, cnt)
