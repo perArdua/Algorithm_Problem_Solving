d98a470e3f8aec412aed66359517737cecc6c446
perArdua <70432496+perArdua@users.noreply.github.com>
2022-02-24 05:26:02 +0900

    imos 사용시 직관적인 의미를 갖도록 if-else 문 수정

--- BOJ/BOJ_3020_firefly.py
+++ BOJ/BOJ_3020_firefly.py
@@ -10,14 +10,14 @@ arr = [0] * h
 
 for i in range(n):
     if i & 1:
+        arr[h - int(input().rstrip())] += 1
+    else:
         arr[0] += 1
         arr[int(input().rstrip())] -= 1
-    else:
-        arr[h - int(input().rstrip())] += 1
-        
+
 for i in range(1, h):
     arr[i] += arr[i - 1]
-    
+
 res = min(arr)
 cnt = arr.count(res)
 
